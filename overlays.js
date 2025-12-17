f5f078ceece93db4122fd50753c96a6853b27313
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2025-12-17 12:21:36 -0600

    texture overaly

--- overlays.js
+++ overlays.js
@@ -4,7 +4,7 @@ const overlays = [
     { name: 'CRT Overlay', path: 'img/overlay/crt-overlay.png' },
     { name: 'Dry Cracks', path: 'img/overlay/dry-cracks.png' },
     { name: 'Grit', path: 'img/overlay/grit.png' },
-    { name: 'Yellow', path: 'img/overlay/yellow.png' }
+    { name: 'Light Grit', path: 'img/overlay/light-grit.png' }
     // Add more overlays here as you add files to img/overlay/
     // Format: { name: 'Texture Name', path: 'img/overlay/texture.png' }
 ];
